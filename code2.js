0de0caacf4171d903e42cfbec339b8cdb75104d1
PhillipOdd <135106680+PhillipOdd@users.noreply.github.com>
2023-07-07 20:30:08 +0300

    final code2.js

--- code2.js
+++ code2.js
@@ -1,18 +1,17 @@
-var speed = 200;
-var speedLimit = 70;
-var points = 5;
-
-    if (speed < 70) {
+//Declaring the function
+function speedDectector(speed) {
+    //Declare the variables
+    var speedLimit = 70;
+    var points = 5;
+    //computing no. of points
+    var points = Math.floor((speed - speedLimit) / 5)
+    if (speed < speedLimit) {
         console.log("ok");
     }
-
-    var points = Math.floor((speed - speedLimit) / 5)
-
-    if (points > 12) {
-        console.log("License suspended");
-
+    else if (points <= 12) {
+        console.log(points + "points");
     }
+    //if not under or equal 12
     else {
-        console.log(points);
+        console.log("your license is suspended");
     }
-    
