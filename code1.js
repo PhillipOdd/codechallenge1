a53ac1d2fceeb76cb4e27a4dfacc280e3dfe2b95
PhillipOdd <135106680+PhillipOdd@users.noreply.github.com>
2023-07-07 13:07:02 +0300

    Add files via upload

---
+++ code1.js
@@ -0,0 +1,25 @@
+//start of first question
+var marks = 35;
+{
+    if (marks <= 100 && marks >= 80) {
+        console.log("Grade : A");
+    }
+    else if (marks <= 79 && marks >= 60) {
+        console.log("Grade : B");
+    }
+    else if (marks <= 59 && marks >= 49) {
+        console.log("Grade : C");
+    }
+    else if (marks <= 48 && marks >= 40) {
+        console.log("Grade : D");
+    }
+    else {
+        console.log("Grade : E");
+    }
+
+
+}
+//end of first question
+
+
+
